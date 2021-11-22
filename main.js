3354164aa802d3c4bd2aaf695f47d06a884f99eb
UrbanoG2 <giuseppe.16.urbano@gmail.com>
2021-11-22 17:28:21 +0100

    using classes

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 // creo una variabile per il tasto "button"
 
 const show = document.querySelector(".header-right > a");
+console.log(show);
 
 // creo una variabile per il menù a scomparsa
 
 const blueBg = document.querySelector(".hamburger-menu");
+console.log(blueBg);
 
 // creo un evento in cui premendo il tasto "button" mi appare il menù 
 
 show.addEventListener ("click", function(){
-    blueBg.style.display = "block"
+    blueBg.classList.add ("active");
     console.log("Now you see the menù");
 })
 
@@ -22,7 +24,7 @@ const close = document.querySelector(".close");
 // creo un evento per il quale cliccando il tasto "close" si chiude il menù
  
 close.addEventListener ("click", function(){
-    blueBg.style.display = "none"
+    blueBg.classList.remove ("active");
     console.log("Now you don't see the menù");
 })
 
